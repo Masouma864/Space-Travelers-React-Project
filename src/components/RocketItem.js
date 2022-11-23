e43770c0a3f83bb5aafcd6cc70cbc31904ac3884
Micheal <oguntayomicheal@gmail.com>
2022-11-23 01:05:00 +0100

    Add reducers and actions to reserve and cancle reservations

--- src/components/RocketItem.js
+++ src/components/RocketItem.js
@@ -1,19 +1,39 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import './css/RocketItem.css';
+import { reserve, cancleRes } from '../redux/rockets';
 
 function Rocket(props) {
+  const dispatch = useDispatch();
   const allRocket = { props };
   const eachRocket = allRocket.props.rocket;
   return (
     <li className="list">
-      <div className="img-div">
+      <figure className="img-div">
         <img src={eachRocket.flickr_images[0]} alt="Rocket" className="each-img" />
-      </div>
-      <div>
+      </figure>
+      <article>
         <h2>{eachRocket.rocket_name}</h2>
-        <p>{eachRocket.description}</p>
-        <button type="button">Reserve</button>
-      </div>
+        <p>
+          <span>
+            {eachRocket.reserved === false ? '' : 'Reserved'}
+          </span>
+          {eachRocket.description}
+        </p>
+        <button
+          type="button"
+          style={{ backgroundColor: `${eachRocket.reserved === true ? 'grey' : '#007bff9c'}` }}
+          onClick={() => {
+            if (!eachRocket.reserved) {
+              dispatch(reserve(eachRocket.id));
+            } else {
+              dispatch(cancleRes(eachRocket.id));
+            }
+          }}
+        >
+          {eachRocket.reserved === true ? 'Cancel Reservations' : 'Reserve Rocket'}
+        </button>
+      </article>
     </li>
 
   );
--- src/redux/rockets.js
+++ src/redux/rockets.js
@@ -2,6 +2,9 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 const FETCH__ROCKET = 'Space-Travelers-React-Project/src/redux/rockets/FETCH__ROCKET';
 const fetchUrl = 'https://api.spacexdata.com/v3/rockets';
+
+const RESERVE__ROCKET = 'Space-Travelers-React-Project/src/redux/rockets/RESERVE__ROCKET';
+const CANCLE__RESERVE = 'Space-Travelers-React-Project/src/redux/rockets/CANCLE__RESERVE';
 // Reducers
 
 const rocketArr = [];
@@ -10,6 +13,16 @@ export const rocketReducer = (state = rocketArr, action) => {
   switch (action.type) {
     case FETCH__ROCKET:
       return action.allRckts;
+    case RESERVE__ROCKET:
+      return state.map((rocket) => {
+        if (rocket.id !== action.id) return rocket;
+        return { ...rocket, reserved: true };
+      });
+    case CANCLE__RESERVE:
+      return state.map((rocket) => {
+        if (rocket.id !== action.id) return rocket;
+        return { ...rocket, reserved: false };
+      });
     default:
       return state;
   }
@@ -20,7 +33,6 @@ export const rocketReducer = (state = rocketArr, action) => {
 export const fetchRockets = createAsyncThunk(FETCH__ROCKET, async (post, { dispatch }) => {
   const response = await fetch(fetchUrl);
   const data = await response.json();
-
   const dataArr = [];
   data.forEach((each) => {
     dataArr.push(
@@ -29,12 +41,26 @@ export const fetchRockets = createAsyncThunk(FETCH__ROCKET, async (post, { dispa
         rocket_name: each.rocket_name,
         description: each.description,
         flickr_images: each.flickr_images,
+        reserved: false,
       },
     );
   });
-  console.log(dataArr);
   dispatch({
     type: FETCH__ROCKET,
-    allRckts: data,
+    allRckts: dataArr,
   });
 });
+
+export const reserve = (id) => (
+  {
+    type: RESERVE__ROCKET,
+    id,
+  }
+);
+
+export const cancleRes = (id) => (
+  {
+    type: CANCLE__RESERVE,
+    id,
+  }
+);
